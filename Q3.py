9996cf0fdb8ad30c0005a1d78b36efa18ba5ae8e
Elad Ben Shabat <88495300+eladsnd@users.noreply.github.com>
2021-10-23 17:56:55 +0300

    made more readable result added Q1_e
    need to check :
    why percentages so low
    
    added Q2

--- Q3.py
+++ Q3.py
@@ -1,5 +1,6 @@
 from scipy.stats import poisson
 from scipy.stats import geom
+
 lamb = 20
 
 
@@ -21,22 +22,30 @@ def lerger_then(large):
 
 
 if __name__ == '__main__':
+
+    # Q3_A_1
     x = smaller_then(0.9)
     print("the index is : " + str(x) + " and the probability of a lower number is : " + str(poisson.cdf(x, lamb)))
+
+    # Q3_A_2
     x = lerger_then(0.9)
     print("the index is : " + str(x - 1) + " and the probability of a larger number is : " + str(
         1 - poisson.cdf(x - 1, lamb)))
 
+    # Q3_A_3
     first_q = smaller_then(0.25)
     last_q = lerger_then(0.25)
     IQR = poisson.cdf(last_q, lamb) - poisson.cdf(first_q, lamb)
     print("the IQR is : " + str(IQR))
 
+    # Q3_A_4
     less_then_16 = poisson.cdf(16, lamb)
     print("the probability for up to 16 new sick people is : p = " + str(less_then_16))
 
-    print("the expected value : " + str(1/less_then_16))
+    # Q3_A_5
+    print("the expected value : " + str(1 / less_then_16))
 
+    # Q3_A_6
     start = 0
     while geom.cdf(start, less_then_16) < 0.5:
         start += 1
